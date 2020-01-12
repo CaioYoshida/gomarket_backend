92c9ba8f9b8feeab5714b430aaa565e64e32822c
Caio Yoshida <caio.yoshida@yahoo.com.br>
2020-01-11 21:37:01 -0300

    chore: JWT validation

--- start/routes.js
+++ start/routes.js
@@ -3,3 +3,6 @@
 const Routes = use('Route')
 
 Routes.post('users', 'UserController.store').validator('User')
+Routes.post('sessions', 'SessionController.store')
+
+Routes.group(() => {}).middleware(['auth'])
