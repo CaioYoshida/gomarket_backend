2a9c975aac1171cc308f8aa1a21a7497f33ef174
Caio Yoshida <caio.yoshida@yahoo.com.br>
2020-01-12 21:54:30 -0300

    chore: add Product Model + add products migration

---
+++ database/migrations/1578875382674_products_schema.js
@@ -0,0 +1,26 @@
+'use strict'
+
+/** @type {import('@adonisjs/lucid/src/Schema')} */
+const Schema = use('Schema')
+
+class ProductsSchema extends Schema {
+  up () {
+    this.create('products', (table) => {
+      table.increments()
+      table.string('product_name', 80).notNullable().unique()
+      table.string('unit_type', 20).notNullable()
+      table.decimal('current_price')
+      table.decimal('last_price')
+      table.date('last_price_date')
+      table.decimal('best_price')
+      table.date('best_price_date')
+      table.timestamps()
+    })
+  }
+
+  down () {
+    this.drop('products')
+  }
+}
+
+module.exports = ProductsSchema
