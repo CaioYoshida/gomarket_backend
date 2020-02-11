08ac8c21198612220a2b33761901f7e7956c84db
Caio Yoshida <caio.yoshida@yahoo.com.br>
2020-02-11 16:08:02 -0300

    inserting lists table inside database

---
+++ database/migrations/1581447757121_list_schema.js
@@ -0,0 +1,21 @@
+'use strict'
+
+/** @type {import('@adonisjs/lucid/src/Schema')} */
+const Schema = use('Schema')
+
+class ListSchema extends Schema {
+  up () {
+    this.create('lists', (table) => {
+      table.increments()
+      table.date('date')
+      table.json('products')
+      table.timestamps()
+    })
+  }
+
+  down () {
+    this.drop('lists')
+  }
+}
+
+module.exports = ListSchema
